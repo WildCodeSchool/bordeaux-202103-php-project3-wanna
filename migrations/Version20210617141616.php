d06a59cbf73b5687417fe44cb58b1e3beb6d3587
Killian <killian.couraillon@hotmail.fr>
2021-06-17 17:42:53 +0200

    front and animation of flashes message, be careful, if several flashes, juste one is displayed because of position fixed

--- migrations/Version20210617141616.php
+++ migrations/Version20210617141616.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210616223032 extends AbstractMigration
+final class Version20210617141616 extends AbstractMigration
 {
     public function getDescription(): string
     {
