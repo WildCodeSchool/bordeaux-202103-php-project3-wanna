10f7516f75cd0d528302d32a3798f323718a5041
Killian <killian.couraillon@hotmail.fr>
2021-07-13 11:30:55 +0200

    front of notifications

---
+++ src/Controller/NotificationController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\NotificationRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NotificationController extends AbstractController
+{
+    public function index(NotificationRepository $notificationRepository): Response
+    {
+        $notifications = $notificationRepository->findBy(['receiver' => $this->getUser()], ['id' => 'DESC']);
+        return $this->render('notification/_index.html.twig', [
+            'notifications' => $notifications,
+        ]);
+    }
+}
