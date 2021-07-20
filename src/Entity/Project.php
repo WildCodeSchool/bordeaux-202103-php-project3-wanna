eb61c65b20513e8460ed15dfefe4eb8078000d35
Killian <killian.couraillon@hotmail.fr>
2021-07-20 17:39:29 +0200

    fix the issue number 1061 and 1063

--- src/Entity/Project.php
+++ src/Entity/Project.php
@@ -138,6 +138,18 @@ class Project
         return [];
     }
 
+    public function hasWaitingVolunteers(): bool
+    {
+        $hasWaitingVolunteer = false;
+        $participants = $this->getParticipants();
+        foreach ($participants as $participant) {
+            if ($participant->getRole() === Participant::ROLE_WAITING_VOLUNTEER) {
+                $hasWaitingVolunteer = true;
+            }
+        }
+        return $hasWaitingVolunteer;
+    }
+
     public function getProjectOwner(): User
     {
         $participants = $this->getParticipants();
